966a66347347b62f96c1b95cf68c65909a915e16
Blowely <maryashin.2014@yandex.ru>
2023-05-28 14:19:30 +0300

    work on product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-import {Layout} from "antd";
+import {Button, Layout} from "antd";
 import {useGetProductQuery} from "../store/products.store";
 import {useSearchParams} from "react-router-dom";
 import CarouselComponent from "../components/Carousel/Carousel";
@@ -22,9 +22,13 @@ function Product({onAddToFavorite, onAddToCart, isLoading}) {
             <div style={{fontSize: '30px', fontWeight: '600'}}>руб {ad?.price}</div>
             <div style={{fontSize: '24px'}}>{ad?.title}</div>
           </div>
+          <div style={{backgroundColor: 'white', margin: '10px', padding: '10px'}}>
+            <div style={{fontSize: '30px', fontWeight: '600'}}>руб {ad?.price}</div>
+            <div style={{fontSize: '24px'}}>{ad?.title}</div>
+          </div>
           <div style={{position: "fixed", marginTop: "calc(100vh - 90px)", width: '100%',
-            height: '90px', backgroundColor: "white", borderTop: '1px solid #f9f9f9'}} >
-
+            height: '90px', padding: 10, backgroundColor: "white", borderTop: '1px solid #f9f9f9'}} >
+            <Button style={{width: '100%', height: '100%'}}>Выбрать размер</Button>
           </div>
         </Layout>
     );
