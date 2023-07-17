f179a361add3808368fce2fec652cd521daceeeb
Blowely <maryashin.2014@yandex.ru>
2023-07-17 21:19:48 +0300

    added some statuses

--- src/pages/constants.js
+++ src/pages/constants.js
@@ -5,3 +5,18 @@ export const addressTypes = {
 export const addressTypesDictionary = {
     BB_PVZ: "Boxberry"
 }
+
+export const PRODUCT_STATUS = {
+    CREATED: 'created',
+    APPROVED: 'approved',
+    PAID: 'paid',
+}
+
+export const PRODUCT_SERVICE_STATUS = {
+    REGISTERED: 'registered',
+    SEND_TO_CHINESE_DELIVERY: 'send_to_chinese_delivery',
+    SORT_RUSSIA: 'sort_russia',
+    SENT_TO_PVZ: 'sent_to_pvz',
+    ARRIVED_IN_PVZ: 'pvz_arrived',
+    RECEIVED: 'received'
+}
