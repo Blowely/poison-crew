38b1635979e35d2f93087e3eaa79a95f8bd40647
Blowely <maryashin.2014@yandex.ru>
2024-07-28 14:51:31 +0300

    removed suspense

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -240,7 +240,7 @@ function Home({ onAddToFavorite, onAddToCart }) {
             <div style={{ fontWeight: "bold", fontSize: "10px" }}>Больше</div>
           </div>
         </div>*/}
-            <Suspense fallback={<div>Loading...</div>}>{renderItems()}</Suspense>
+            {renderItems()}
           </div>
           {!isDesktopScreen &&
             <footer>
