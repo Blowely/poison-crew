d5937eae8f633534e9ef8fe7ddb57c1f6702a4c2
Blowely <maryashin.2014@yandex.ru>
2024-08-08 17:07:00 +0300

    added list sizes

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./Filters.scss";
 import { Input } from "antd";
 
 
 function Filters() {
+  const [choice, setChoice] = useState({});
+  const [sizes, setSizes] = useState([]);
 
   const isDesktopScreen = window.screen.availWidth > 600;
 
+  useEffect(() => {
+    if (!sizes?.length) {
+      const euSizes = []
+      for (let i = 36; i <= 52.5; i+=0.5) {
+        euSizes.push(i);
+      }
+      setSizes(euSizes)
+    }
+  },[sizes?.length])
+
+  const onChangeChoiceHandler = (el) => {
+    console.log('el=',el);
+    setChoice(el);
+  };
+
   return (
-    <div>
-      <div className="prices-wrapper">
+    <div className="params-wrapper">
+      <div className="params-item-wrapper">
         <div className="param-title">
           Цена, RUB
         </div>
@@ -20,7 +37,36 @@ function Filters() {
           <Input size="large" placeholder="520433" prefix="до" suffix="₽" />
         </div>
       </div>
+      <div className="params-item-wrapper">
+        <div className="param-title">
+          Размеры, EU
+        </div>
 
+        <div className="list">
+          {sizes?.map((el, i) => (
+            <div
+              className={
+                el === choice
+                  ? "size-wrapper gap-2 selected"
+                  : "size-wrapper gap-2"
+              }
+              onClick={() => onChangeChoiceHandler(el)}
+              key={i}
+              role="presentation"
+            >
+              <div
+                style={{
+                  fontSize: "15px",
+                  fontWeight: "400",
+                  textAlign: "center",
+                }}
+              >
+                {el}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
