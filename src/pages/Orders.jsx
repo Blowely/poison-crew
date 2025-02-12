aab7236c6ada27ad3b62485c6bffc86d8aa25232
maryashin.andrey <maryashin.andrey@wb.ru>
2025-02-12 15:32:22 +0300

    changed footer icons

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -23,6 +23,7 @@ const Orders = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const from = searchParams.get('from');
     const token = localStorage.getItem('token');
+    const gender = localStorage.getItem("gender");
 
     const {data: accountData, isLoading: isLoadingAcc, error: accError} = useGetAccountQuery(token);
     const clientId = accountData?.account?._id;
@@ -172,18 +173,34 @@ const Orders = () => {
 
         {!isDesktopScreen &&
             <footer>
-                <div onClick={() => navigate('/products')}>
-                    <NonActiveBagIcon/>
+                <div onClick={() => navigate("/products")}>
+                    <img style={{height: '50px'}}
+                         src="https://storage.yandexcloud.net/pc-mediafiles/icons/1.%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F.png"
+                         alt=""/>
                 </div>
-                <div onClick={() => navigate('/cart?from=products')}>
-                    <NonActiveCartIcon style={{fontSize: '30px'}}/>
+                <div onClick={() => navigate(`/${gender}/categories/`)}>
+                    <img style={{height: '50px'}}
+                         src="https://storage.yandexcloud.net/pc-mediafiles/icons/2.%D0%9A%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3.png"
+                         alt=""/>
                 </div>
-                <div onClick={() => navigate('/profile')}>
-                    <ActiveProfileIcon style={{fontSize: '30px'}}/>
+                <div onClick={() => navigate("/cart?from=products")}>
+                    <img style={{height: '50px'}}
+                         src="https://storage.yandexcloud.net/pc-mediafiles/icons/3.%D0%9A%D0%BE%D1%80%D0%B7%D0%B8%D0%BD%D0%B0.png"
+                         alt=""/>
+                </div>
+                <div onClick={() => navigate("/favorites")}>
+                    <img style={{height: '50px'}}
+                         src="https://storage.yandexcloud.net/pc-mediafiles/icons/4.%D0%98%D0%B7%D0%B1%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5.png"
+                         alt=""/>
+                </div>
+                <div onClick={() => navigate("/profile")}>
+                    <img style={{height: '50px'}}
+                         src="https://storage.yandexcloud.net/pc-mediafiles/icons/5.%D0%9F%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8C%20%D0%B0%D0%BA%D1%82%D0%B8%D0%B2.png"
+                         alt=""/>
                 </div>
             </footer>
         }
-    </Layout>
+        </Layout>
     );
 }
 export default Orders;
