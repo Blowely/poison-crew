9ffb6beaa0ea0d478d74d839f96a9b9ac156529e
maryashin.andrey <maryashin.andrey@wb.ru>
2025-02-13 18:57:57 +0300

    clean code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,11 @@
-import {Link, useSearchParams} from "react-router-dom";
-import {Button, Input, Segmented} from "antd";
+import {useSearchParams} from "react-router-dom";
+import {Button, Input} from "antd";
 import React, {useEffect, useMemo, useState} from "react";
-import { FilterFilled, FilterOutlined, SearchOutlined } from "@ant-design/icons";
 import './header.styles.scss';
 import {useGetCollectionsQuery} from "../../store/collections.store";
 import {getMultipleRandom} from "../../common/utils";
-import RePoizonMainLogo from "../../assets/svg/re-poizon-main-logo.js"
 
-const Header = ({search, showFilters, setShowFilters, setOffset, setLoading, isEnabledFilters}) => {
+const Header = ({search, setShowFilters, setOffset, setLoading}) => {
     const [searchParams, setSearchParams] = useSearchParams();
 
     const [searchValue, setSearchValue] = useState(search || '');
@@ -107,32 +105,6 @@ const Header = ({search, showFilters, setShowFilters, setOffset, setLoading, isE
               </Button>
             }
         </div>
-
-        {/*<Segmented className="header-segmented mt-15 w100p"
-                   onChange={onChangeCollection} options={['Для Вас', 'Популярное', ...collectionsNames]} />*/}
-        {/*<Link to="/">
-                <div className="d-flex align-center">
-                    <svg width={40} height={40} src="svg/logo.png" alt="Logo" />
-                    <div>
-                        <h3 className="text-uppercase">Sneaker Spot</h3>
-                        <p className="opacity-5">Best Sneakers-shop EUW</p>
-                    </div>
-                </div>
-            </Link>*/}
-        {/*<ul className="d-flex">
-                <li onClick={props.onClickCart} className="mr-30 cu-p">
-                    <svg width={18} height={18} src="svg/cart.svg" alt="trashcan" />
-                    <span>1205 eur.</span>
-                </li>
-                <li className="mr-20 cu-p">
-                    <Link to ="/favorites">
-                        <svg width={18} height={18} src="svg/heart.svg" alt="favorites" />
-                    </Link>
-                </li>
-                <li>
-                    <svg width={18} height={18} src="svg/user.svg" alt="user" />
-                </li>
-            </ul>*/}
     </header>
     );
 }
