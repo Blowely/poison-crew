bd30de6fea1c2b5b382bbf33a02c395972d95be4
maryashin.andrey <maryashin.andrey@wb.ru>
2025-02-20 19:41:22 +0300

    bugfix

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -218,6 +218,16 @@ function Product({ selectedProduct, setLoading }) {
     }
   }
 
+  console.log('window.history',window.history)
+
+  const goBack = () => {
+    if (window?.history?.length === 2) {
+      return navigate("/products");
+    }
+
+    window.history.go(-1);
+  }
+
   const isDesktopScreen = window?.innerWidth > 768;
 
   return (
@@ -296,8 +306,7 @@ function Product({ selectedProduct, setLoading }) {
           <div style={{height: '100%'}}>
             <img src="https://storage.yandexcloud.net/pc-mediafiles/icons/%D0%9D%D0%B0%D0%B7%D0%B0%D0%B4%20(1).png"
                  alt="" className="go-back-btn"
-                 onClick={() => window.history.go(-1)}/>
-
+                 onClick={goBack} />
 
             <img
                 src="https://storage.yandexcloud.net/pc-mediafiles/icons/%D0%9F%D0%BE%D0%B4%D0%B5%D0%BB%D0%B8%D1%82%D1%8C%D1%81%D1%8F(cropped).png"
