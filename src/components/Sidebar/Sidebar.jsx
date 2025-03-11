2550cea960550f46c4c0406834663d73e1d35d0c
maryashin.andrey <maryashin.andrey@wb.ru>
2025-03-11 13:11:42 +0300

    clean code

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -16,28 +16,6 @@ const Sidebar = ({visible, setVisibleCategories, setOffset, setLoading}) => {
         navigate(`?category${props?.categoryLvl}Id=${props.id}`);
     }
 
-    function addLabelField(categories) {
-        return categories.map(category => {
-            // Добавляем поле label, равное полю name
-            if (category?.id) {
-                category.key = category.id;
-            }
-
-
-            // Если есть вложенные элементы, рекурсивно обрабатываем их
-            if (category.children && category.children.length > 0) {
-                category.children = addLabelField(category.children);
-            }
-
-            return category;
-        });
-    }
-
-// Добавляем поле label ко всем объектам
-    /*const FILLED_CATEGORIES_WITH_LABEL = addLabelField(FILLED_CATEGORIES);
-
-    console.log(FILLED_CATEGORIES_WITH_LABEL);*/
-
     return (
         <div className="sidebar-container">
             <Drawer title="Меню" placement="left" onClose={() => setVisibleCategories(false)} visible={visible}>
