96e2c854c0e8a72e044165730033fb9a40158236
maryashin.andrey <maryashin.andrey@wb.ru>
2025-03-11 14:06:08 +0300

    experiment

--- src/components/Carousel/SwiperCarousel.js
+++ src/components/Carousel/SwiperCarousel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import {Navigation, Pagination, Zoom} from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from "./index.module.scss"
@@ -11,9 +11,28 @@ const SwiperCarousel = (props) => {
   const {images, onLoad, onError, lazyPreloadPrevNext, loop} = props;
   const isDesktopScreen = window.screen.availWidth > 600;
 
+  const swiperRef = useRef(null);
+
+    const onSwiper = (swiper) => {
+        swiper.on('zoomChange', (scale) => {
+            swiper.allowTouchMove = scale === 1; // Блокируем свайп при увеличении
+            swiper.allowSlideNext = scale === 1;
+            swiper.allowSlidePrev = scale === 1;
+        });
+    };
+
   return (
-    <Swiper pagination={true} zoom={true} navigation={true} modules={[Pagination, Navigation, Zoom]} style={{width:'100%'}}
-            lazyPreloadPrevNext={lazyPreloadPrevNext} loop={loop}>
+    <Swiper
+        onSwiper={onSwiper}
+        pagination={true} zoom={true} navigation={true}
+        modules={[Pagination, Navigation, Zoom]}
+        style={{
+          width:'100%',
+          touchAction: 'none'
+        }}
+        lazyPreloadPrevNext={lazyPreloadPrevNext} loop={loop}
+        ref={swiperRef}>
+    >
       {images?.map((image, index) => {
         return (
           <SwiperSlide key={index} zoom={true} modules={[Zoom]} className={!isDesktopScreen && styles.slide}>
