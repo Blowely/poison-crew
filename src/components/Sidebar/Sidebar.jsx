b8156d5601365bdd2a7d8a5620478b418b92c2a7
maryashin.andrey <maryashin.andrey@wb.ru>
2025-03-24 16:04:27 +0300

    sidebar scroll fix

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,52 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Menu, Drawer, Image } from "antd";
 import "./Sidebar.scss";
-import {useNavigate} from "react-router-dom";
-import {FILLED_CATEGORIES} from "../constants";
+import { useNavigate } from "react-router-dom";
+import { FILLED_CATEGORIES } from "../constants";
 
-const Sidebar = ({visible, setVisibleCategories, setOffset, setLoading}) => {
+const Sidebar = ({ visible, setVisibleCategories, setOffset, setLoading }) => {
     const navigate = useNavigate();
 
-    const onCategoryClick = ({item}) => {
-        const {props} = item;
+    useEffect(() => {
+        if (visible) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, [visible]);
+
+    const onCategoryClick = ({ item }) => {
+        const { props } = item;
 
         setOffset(0);
         setLoading(true);
         setVisibleCategories(false);
         navigate(`?category${props?.categoryLvl}Id=${props.id}`);
-    }
-
-    // Функция для рендеринга кастомных элементов меню
-    const renderMenuItem = (item) => {
-        return (
-            <div style={{ display: 'flex', alignItems: 'center', gap: '10px',  fontSize: '17px'}}>
-                {item.img && (
-                    <Image
-                        src={item.img}
-                        width={24}
-                        height={24}
-                        preview={false}
-                        style={{
-                            marginRight: 8,
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            borderRadius: '10px'
-                        }}
-                    />
-                )}
-                <span>{item.label}</span>
-            </div>
-        );
     };
+
+    const renderMenuItem = (item) => (
+        <div style={{ display: 'flex', alignItems: 'center', gap: '10px', fontSize: '17px' }}>
+            {item.img && (
+                <Image
+                    src={item.img}
+                    width={24}
+                    height={24}
+                    preview={false}
+                    style={{
+                        marginRight: 8,
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        borderRadius: '10px'
+                    }}
+                />
+            )}
+            <span>{item.label}</span>
+        </div>
+    );
 
     return (
         <div className="sidebar-container">
-            <Drawer title="Каталог" placement="left" onClose={() => setVisibleCategories(false)} visible={visible}>
+            <Drawer
+                title="Каталог"
+                placement="left"
+                onClose={() => setVisibleCategories(false)}
+                visible={visible}
+                bodyStyle={{ overflowY: "auto", maxHeight: "100vh" }} // Даем возможность скроллить сайдбар
+            >
                 <Menu
                     mode="inline"
                     onClick={onCategoryClick}
-                    // Кастомизация рендеринга элементов меню
                     items={FILLED_CATEGORIES.map(item => ({
                         ...item,
                         label: renderMenuItem(item),
