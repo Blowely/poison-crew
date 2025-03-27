513807f4977d143119b5f764c47286862a00c206
maryashin.andrey <maryashin.andrey@wb.ru>
2025-03-27 13:43:55 +0300

    hover on header icons

--- src/components/MainLogoComponent/MainLogoComponent.jsx
+++ src/components/MainLogoComponent/MainLogoComponent.jsx
@@ -27,6 +27,27 @@ const MainLogoComponent = ({setOffset, setLoading, style}) => {
         window.open(link);
     }
 
+    const val11 = "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/5.%D0%9F%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8C.png"
+    const val12 = "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/5.%D0%9F%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8C%20%D0%B0%D0%BA%D1%82%D0%B8%D0%B2.png"
+
+    const val21 = "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/4.%D0%98%D0%B7%D0%B1%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5.png"
+    const val22 = "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/4.%D0%98%D0%B7%D0%B1%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%B0%D0%BA%D1%82%D0%B8%D0%B2.png"
+
+    const val31 = "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/3.%D0%9A%D0%BE%D1%80%D0%B7%D0%B8%D0%BD%D0%B0.png"
+    const val32 = "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/3.%D0%9A%D0%BE%D1%80%D0%B7%D0%B8%D0%BD%D0%B0%20%D0%B0%D0%BA%D1%82%D0%B8%D0%B2.png"
+
+    const [profileIcon, setProfileIcon] = useState(val11)
+    const [favIcon, setFavIcon] = useState(val21)
+    const [cartIcon, setCartIcon] = useState(val31)
+
+    const onMouseOver = (setIcon, val) => {
+        setIcon(val);
+    }
+
+    const onMouseLeave = (setIcon, val) => {
+        setIcon(val);
+    }
+
     return (<>
         {!token && openAuth &&
             <AuthModal
@@ -69,21 +90,33 @@ const MainLogoComponent = ({setOffset, setLoading, style}) => {
                 <div className="actions-btns" style={{width: "80%"}}>
                     <GenderSwitcher setOffset={setOffset} setLoading={setLoading}/>
                     <div className="items-wrapper">
-                        <div className="item" onClick={onProfileClick}>
+                        <div className="item"
+                             onClick={onProfileClick}
+                             onMouseOver={() => onMouseOver(setProfileIcon, val12)}
+                             onMouseLeave={() => onMouseLeave(setProfileIcon, val11)}
+                        >
                             <img style={{height: '23px'}}
-                                 src="https://storage.yandexcloud.net/pc-mediafiles/icons/v2/5.%D0%9F%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8C.png"
+                                 src={profileIcon}
                                  alt=""/>
                             Профиль
                         </div>
-                        <div className="item" onClick={() => navigate("/favorites")}>
+                        <div className="item"
+                             onClick={() => navigate("/favorites")}
+                             onMouseOver={() => onMouseOver(setFavIcon, val22)}
+                             onMouseLeave={() => onMouseLeave(setFavIcon, val21)}
+                        >
                             <img style={{height: '23px'}}
-                                 src="https://storage.yandexcloud.net/pc-mediafiles/icons/v2/4.%D0%98%D0%B7%D0%B1%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5.png"
+                                 src={favIcon}
                                  alt=""/>
                             Избранное
                         </div>
-                        <div className="item" onClick={() => navigate("/cart")}>
+                        <div className="item"
+                             onClick={() => navigate("/cart")}
+                             onMouseOver={() => onMouseOver(setCartIcon, val32)}
+                             onMouseLeave={() => onMouseLeave(setCartIcon, val31)}
+                        >
                             <img style={{height: '23px'}}
-                                 src="https://storage.yandexcloud.net/pc-mediafiles/icons/v2/3.%D0%9A%D0%BE%D1%80%D0%B7%D0%B8%D0%BD%D0%B0.png"
+                                 src={cartIcon}
                                  alt=""/>
                             Корзина
                         </div>
